57429e8db9c89633eaf65d8e7e1182bbb7addb1d
Rup medhi <rupmmedhi@gmail.com>
2023-06-11 18:02:00 +0530

    onAuthStateChange & check user login or not correct

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import NavPhoto from './Components/NavPhoto';
-import FeatureCard from './Components/FeatureCard';
 import Products from './Components/Products';
 import Footer from './Components/Footer';
 import SignUpForm from './Components/SignUpForm';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-
-} from "react-router-dom";
 import AllProducts from './Components/SeeAllProducts/AllProducts';
 import Cart from './Components/Shopping-Cart/Cart';
 import Login from './Components/Login';
@@ -30,39 +16,42 @@ import { useDispatch } from 'react-redux';
 import { checkLogin } from './Components/Redux/Slices/isLoginSlices';
 import { auth } from './Components/Firebase/FireBase';
 
-
 function App() {
   const [user, setUser] = useState(null);
   const dispatch = useDispatch();
 
-  useEffect(async () => {
-    try {
-      const unsubscribe = auth.onAuthStateChanged((user) => {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
         setUser(user);
-        dispatch(checkLogin(user))
-      });
-      return () => unsubscribe();
-    } catch (err) {
-      alert(arr.massage)
-    }
+        dispatch(checkLogin(user));
+      },
+      (error) => {
+        console.log(error);
+       
+      }
+    );
+
+    return () => unsubscribe();
   }, []);
-  console.log(user)
 
   return (
     <Router>
       <ChakraProvider theme={theme}>
         <Navbar />
         <Routes>
-          <Route path="/" element={
-            <>
-              <NavPhoto />
-              <Products />
-            </>}
+          <Route
+            path="/"
+            element={
+              <>
+                <NavPhoto />
+                <Products />
+              </>
+            }
           />
-          <Route path='/allProducts' element={<AllProducts />} />
-          <Route path='/shoppingcart' element={<Cart />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUpForm />} />
+          <Route path="/allProducts" element={<AllProducts />} />
+          <Route path="/shoppingcart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUpForm />} />
         </Routes>
         <Footer />
       </ChakraProvider>
